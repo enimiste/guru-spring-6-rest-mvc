5e8c61688876b9fa03d5b5c5fc9e81e55121854a
enimiste <nouni.elbachir@gmail.com>
2024-12-05 15:49:47 +0100

    handle post request for creating beer

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -4,11 +4,10 @@ import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 import java.util.UUID;
 
@@ -33,6 +32,12 @@ public class BeerController {
         log.debug("Get Beer by Id - in controller");
 
         return beerService.getBeerById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createBeer(@RequestBody Beer beer){
+        Beer savedBeer = beerService.saveNewBeer(beer);
+        return ResponseEntity.created(URI.create("http://localhost:8080/api/v1/beer/%s".formatted(savedBeer.getId()))).build();
     }
 
 }
